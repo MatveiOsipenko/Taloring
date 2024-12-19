432c9d946447509bdfdef695b63cc53dd465e9dd
MatveiOsipenko <thetimegoesby@mail.ru>
2024-12-19 12:56:35 +0500

    Add files via upload

---
+++ connect_db.php
@@ -0,0 +1,7 @@
+<?php
+$host = ""; // Имя хоста
+$login = ""; // Логин
+$pass = ""; // Пароль
+$db_name = ""; // Имя базы данных
+$mysql = new mysqli($host,$login,$pass,$db_name); // Подключение к БД
+?>
